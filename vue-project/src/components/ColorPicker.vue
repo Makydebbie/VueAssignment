3de0f4cf79ea8d3670adab6991d41534cefcf14a
Dzifa <dzifamakafui@gmail.com>
2023-02-05 23:35:04 +0000

    first commit

---
+++ vue-project/src/components/ColorPicker.vue
@@ -0,0 +1,17 @@
+<template>
+  <div> <h1>Color Picker Game</h1>
+  
+    <div>{{ message }}</div>
+  
+    <button v-for="color in colors" :key="color" @click="matchColor(color)">
+      {{ color }}
+    </button></div> 
+  </template>
+  
+  <script setup>
+
+  import {useRandomColorPicker} from '../composables/ColorPicker'
+  const { colors, message, matchColor } = useRandomColorPicker()
+  </script>
+  
+  <style></style>
---
+++ vue-project/src/composables/ColorPicker.js
@@ -0,0 +1,15 @@
+import { ref } from "vue";
+export function useRandomColorPicker() {
+    const colors = ref(["green", "red", "blue", "purple"]);
+      let message = ref("Pick a color...");
+      const matchColor = (value) => {
+        // do a random color based on the array index;
+        const randomNumber = Math.floor(Math.random() * 3) + 1; //between 1 - 4
+        if (colors.value[randomNumber] === value) {
+          message.value = `You win... [answer: ${colors.value[randomNumber]}]`;
+          return;
+        }
+        message.value = `You lose [answer: ${colors.value[randomNumber]}]`;
+      };
+      return { colors, message, matchColor };
+}
